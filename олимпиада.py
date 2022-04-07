9c486259e644cf6648e64182532838acbaeb1cca
s7plc <90773497+s7plc@users.noreply.github.com>
2022-04-07 22:58:47 +0300

    Update олимпиада.py
    from dad

--- олимпиада.py
+++ олимпиада.py
@@ -1,5 +1,11 @@
-
-a = 111
-
-print(f"Привет!{a}")
-
+n = int(input('Enter cabins amount\n'))
+x = int(input('Enter cabin number\n'))
+y = (n - x) - x + 1
+if n < x or x <= 0 or n <= 0:
+    print('stupid combination')
+elif y == 0:
+    print("You reach your cabin")
+elif y < 0:
+    print(f"go backward {y * -1} cabins")
+else:
+    print(f'go forward {y} cabins')
